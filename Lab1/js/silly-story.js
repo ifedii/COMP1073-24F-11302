bc158fe9db3e26fc7061a79027080fe0107320ba
ifedii <ifedioranma2006@gmail.com>
2024-09-23 15:44:31 -0400

    edited js

--- Lab1/js/silly-story.js
+++ Lab1/js/silly-story.js
@@ -60,10 +60,13 @@ function result() {
 	/* STEP 11: If the metric radio button has been checked, we need to convert the temperature and mass numbers in the story */
   if(document.getElementById("metric").checked) {
 		// STEP 11a: Create a variable called weight and convert the 300lbs to kgs (1lb = 0.453592kg)
+		let weight = (300/11) * 0.453592;
 		// STEP 11b: Replace the string 300 pounds with the updated weight in kg
+		newStory = newStory.replace("300 pounds", `${weight}kg`);
+
 
 		// STEP 12a: Create a variable called temp and convert °F to °C ... the formula for conversion is °C = (°F - 32) x 5/9
-
+		let temp = (94 -32) * 5/9;
 		// STEP 12b: Replace the string '94 fahrenheit' with the updated temperature in °C
 
   }

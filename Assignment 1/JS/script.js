5451cbf66874882b1eb951d871dc60df89fda164
ifedii <ifedioranma2006@gmail.com>
2024-09-30 11:21:02 -0400

    added capture function

--- Assignment 1/JS/script.js
+++ Assignment 1/JS/script.js
@@ -1,11 +1,13 @@
 let storyResult = document.querySelector("#storyResult")
 let buttons = document.querySelectorAll(".cycle")
+let displayResult = document.querySelector("#result")
 
 let characters = ["Donald Trump", "Jackie Chan", "Santa Claus", "Will Smith"]
 let actions = ["ran", "saw", "kicked", "pushed"]
 let descriptions = ["a white", "an angry", "a scary", "a funny"]
 let sidekicks = ["Joe Biden", "chicken", "ghost", "frog"];
 let settings = ["at the ball room", "in the church"]
+let story = ":character: :action: :description: :sidekick: :setting:";
 
 let charIndex = 0;
 let actionIndex = 0;
@@ -16,12 +18,13 @@ let settingIndex = 0;
 // storyResult.textContent = "Hello";
 
 buttons.forEach(button => {
+
     button.addEventListener("click", (event) => cycleArray(event.target.textContent))
     
 });
 
 function cycleArray(sentencePart){
-    storyResult.textContent = sentencePart;
+    // storyResult.textContent = sentencePart;
     let word;
     switch (sentencePart){
         case "Character":
@@ -55,6 +58,35 @@ function cycleArray(sentencePart){
 
     }
 
-    storyResult.textContent = word;
+    capture(sentencePart, word)
 
 }
+
+function capture(sentencePart, word){
+    switch (sentencePart){
+        case "Character":
+            story = story.replace(":character:", word)
+            break;
+        case "Action":
+            story = story.replace(":character:", word)
+            break;
+        case "Description":
+            story = story.replace(":character:", word)
+            break;
+        case "Setting":
+            story = story.replace(":character:", word)
+            break;
+        case "Sidekick":
+            story = story.replace(":character:", word)
+            break;
+        default:
+            storyResult = "an error occured"
+        
+    
+
+    }
+}
+
+displayResult.addEventListener("click", () => {
+    storyResult = story
+})

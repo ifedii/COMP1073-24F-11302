f3cfa46b8fddb46454b5ba28a3b8b115690a1993
ifedii <ifedioranma2006@gmail.com>
2024-09-30 13:27:43 -0400

    changed structure

--- Assignment 1/JS/script.js
+++ Assignment 1/JS/script.js
@@ -1,3 +1,4 @@
+// html variables
 let storyResult = document.querySelector("#storyResult")
 let buttons = document.querySelectorAll(".cycle")
 let displayResult = document.querySelector("#result")
@@ -5,6 +6,7 @@ let selection = document.querySelector("#selection")
 let resetBtn = document.querySelector("#reset")
 let randomize = document.querySelector("#randomize")
 
+// array of selections
 let characters = ["Donald Trump", "Jackie Chan", "Santa Claus", "Will Smith"]
 let actions = ["ran", "saw", "kicked", "pushed"]
 let descriptions = ["a white", "an angry", "a scary", "a funny"]
@@ -12,11 +14,18 @@ let sidekicks = ["Joe Biden", "chicken", "ghost", "frog"];
 let settings = ["at the ball room", "in the church"]
 let story = ":character: :action: :description: :sidekick: :setting:";
 
+// index to keep track of current selection
 let charIndex = 0;
 let actionIndex = 0;
 let descIndex = 0;
 let sidekickIndex = 0;
 let settingIndex = 0;
+
+let currentChar = characters[charIndex]
+let currentAction = actions[actionIndex]
+let currentDesc = descriptions[descIndex]
+let currentSidekick = sidekicks[sidekickIndex]
+let currentSetting = settings[settingIndex]
 
 // add event lister each button and pass in the text content to the cycle array function
 buttons.forEach(button => {
@@ -30,88 +39,51 @@ function cycleArray(sentencePart){
     let word;
     switch (sentencePart){
         case "Character":
-            word = characters[charIndex];
+            currentChar = characters[charIndex];
+            word = currentChar;
             charIndex = (charIndex + 1) % characters.length
             break;
         case "Action":
-            word = actions[actionIndex];
+            currentAction = actions[actionIndex]
+            word = currentAction
             actionIndex = (actionIndex + 1) % actions.length
 
             break;
         case "Description":
-            word = descriptions[descIndex];
+            currentDesc = descriptions[descIndex]
+            word = currentDesc
             descIndex = (descIndex + 1) % descriptions.length
 
             break;
         case "Setting":
-            word = settings[settingIndex];
+            currentSetting = settings[settingIndex];
+            word = currentSetting
             settingIndex = (settingIndex + 1) % settings.length
 
             break;
         case "Sidekick":
-            word = sidekicks[sidekickIndex];
+            currentSidekick = sidekicks[sidekickIndex]
+            word = currentSidekick
             sidekickIndex = (sidekickIndex + 1) % sidekicks.length
-
             break;
         default:
             word = "";
 
     }
-
     document.querySelector(`#${sentencePart.toLowerCase()}`).textContent = word;
-    selection.textContent = `${sentencePart}: ${word}`;
-    capture(sentencePart, word)
 
 }
 
-function capture(sentencePart, word){
-    switch (sentencePart){
-        case "Character":
-            story = story.replace(":character:", word)
-            break;
-        case "Action":
-            story = story.replace(":action:", word)
-            break;
-        case "Description":
-            story = story.replace(":description:", word)
-            break;
-        case "Setting":
-            story = story.replace(":setting:", word)
-            break;
-        case "Sidekick":
-            story = story.replace(":sidekick:", word)
-            break;
-        default:
-            story = "an error occured"; 
-
-    }
-}
-
-// display the story on the page
+// display result to the page
 displayResult.addEventListener("click", () => {
-    if (story.includes(":")){
-        storyResult.textContent = "Pls complete your story";
+    storyResult.textContent = `${currentChar} ${currentAction} ${currentDesc} ${currentSidekick} ${currentSetting} `;
 
-    } else{
-        storyResult.textContent = story;
-        story = ":character: :action: :description: :sidekick: :setting:";
-    }
 })
 
 // reset funtion to reset the story on the page and the local variables
-function resetStory(){
-    console.log("Hello");
-    charIndex = 0;
-    actionIndex = 0;
-    descIndex = 0;
-    sidekickIndex = 0;
-    settingIndex = 0;
-    // storyResult.textContent = "";
-    story = ":character: :action: :description: :sidekick: :setting:";
-
-}
-
-// resetBtn.addEventListener("click", resetStory)
+resetBtn.addEventListener("click", () => {
+    storyResult.textContent = "";
+})
 
 
 // random story

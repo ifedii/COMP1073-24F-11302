9b3f6cefd77af464bb998c6ac15d17eeb9f436f4
ifedii <ifedioranma2006@gmail.com>
2024-09-30 11:07:40 -0400

    edited cycleArray() funtion

--- Assignment 1/JS/script.js
+++ Assignment 1/JS/script.js
@@ -1,6 +1,18 @@
 let storyResult = document.querySelector("#storyResult")
 let buttons = document.querySelectorAll(".cycle")
 
+let characters = ["Donald Trump", "Jackie Chan", "Santa Claus", "Will Smith"]
+let actions = ["ran", "saw", "kicked", "pushed"]
+let descriptions = ["a white", "an angry", "a scary", "a funny"]
+let sidekicks = ["Joe Biden", "chicken", "ghost", "frog"];
+let settings = ["at the ball room", "in the church"]
+
+let charIndex = 0;
+let actionIndex = 0;
+let descIndex = 0;
+let sidekickIndex = 0;
+let settingIndex = 0;
+
 // storyResult.textContent = "Hello";
 
 buttons.forEach(button => {
@@ -10,5 +22,39 @@ buttons.forEach(button => {
 
 function cycleArray(sentencePart){
     storyResult.textContent = sentencePart;
+    let word;
+    switch (sentencePart){
+        case "Character":
+            word = characters[charIndex];
+            charIndex = (charIndex + 1) % characters.length
+            break;
+        case "Action":
+            word = actions[actionIndex];
+            actionIndex = (actionIndex + 1) % actions.length
+
+            break;
+        case "Description":
+            word = descriptions[descIndex];
+            descIndex = (descIndex + 1) % descriptions.length
+
+            break;
+        case "Setting":
+            word = settings[settingIndex];
+            settingIndex = (settingIndex + 1) % settings.length
+
+            break;
+        case "Sidekick":
+            word = sidekicks[sidekickIndex];
+            sidekickIndex = (sidekickIndex + 1) % sidekicks.length
+
+            break;
+        default:
+            word = "";
+        
+    
+
+    }
+
+    storyResult.textContent = word;
 
 }

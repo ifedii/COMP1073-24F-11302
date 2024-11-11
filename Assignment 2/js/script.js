78fa768c5bc7afb9e606c3be8d63a212afc378ed
ifedii <ifedioranma2006@gmail.com>
2024-11-10 19:31:51 -0500

    added smoothie class

---
+++ Assignment 2/js/script.js
@@ -0,0 +1,11 @@
+class Smoothie {
+    constructor(flavor, size, addons, quantity, customerName, email) {
+        this.flavor = flavor;
+        this.size = size;
+        this.addons = addons;
+        this.quantity = quantity;
+        this.customerName = customerName;
+        this.email = email;
+    }
+}
+
